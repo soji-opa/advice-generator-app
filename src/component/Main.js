43e71bb7cd2d9f8eaea9d3610d031a48e2d3af50
Azeez Olasoji <azeezolasoji@gmail.com>
2023-02-08 10:54:49 +0200

    show starter jsx code for this project

---
+++ src/component/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Main() {
+	return (
+		<div className='main'>
+			<p className='adviceNo'>Advice #117</p>
+			<p className='advice'>
+				"Its easy to sit up and take notice, what's difficult is getting back up
+				and taking action"
+			</p>
+			<section className='wrapper'>
+				<div className='pause'></div>
+			</section>
+		</div>
+	);
+}
