f333b745823925b7583705e6c97c11b6e9034820
Azeez Olasoji <azeezolasoji@gmail.com>
2023-02-14 08:56:44 +0200

    run the simple api set up for this web app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
 import Main from './component/Main';
 
 function App() {
+	const [apiData, setApiData] = React.useState({});
+
+	React.useEffect(() => {
+		fetch('https://api.adviceslip.com/advice')
+			.then((res) => res.json())
+			.then((data) => setApiData(data.slip));
+	}, []);
+
+	console.log(apiData);
+
+	// React.useEffect(() => {
+	// 	async function getAdvice() {
+	// 		const res = await fetch('https://api.adviceslip.com/advice');
+	// 		const data = await res.join();
+	// 		setApiData(data);
+	// 	}
+	// 	getAdvice();
+	// });
+
 	return (
 		<div className='App'>
-			<Main />
+			<Main advice={apiData.advice} num={apiData.id} />
 		</div>
 	);
 }
